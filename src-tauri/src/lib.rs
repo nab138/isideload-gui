bfad18402588c555f09596f7ca0dcc71d5117232
nab138 <nab@nabdev.me>
2025-11-14 21:48:15 -0500

    actually isideload v0.1.16

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -62,7 +62,7 @@ async fn install_app(
 
     let config = SideloadConfiguration::default()
         .set_store_dir(handle.path().app_config_dir().map_err(|e| e.to_string())?)
-        .set_logger(Box::new(logger));
+        .set_logger(&logger);
 
     let app_path_buf = match Url::parse(&app_path) {
         Ok(url) => {
